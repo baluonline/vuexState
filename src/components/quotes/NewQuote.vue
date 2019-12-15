d90cba1f2f2e89957aa8a2229f9edf4bf7f471ab
baluonline <balurides@gmail.com>
2019-12-15 12:41:22 -0600

    deleted helloworld.vue file

---
+++ src/components/quotes/NewQuote.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+          <button class="primary add-quote" @click.prevent="createNew">
+              Add Quote
+            </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            quote:''
+        }
+    },
+    methods:{
+        createNew(){
+            this.$emit("quoteAdded",this.quote);
+            this.quote="";
+        }
+    }
+
+
+};
+
+
+
+</script>
+
+<style>
+.add-quote{
+    background-color: blue;
+    color: white;
+    
+}
+</style>
